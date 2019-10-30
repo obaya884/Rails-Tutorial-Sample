3826b5c386f4b1ee26d22ae9d9c09c0f07f08fbe
Obaya884 <taku3.884@gmail.com>
2019-10-30 09:32:25 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,24 +1,48 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
   
-  test "layout links" do
+  test "layout links when not logged in" do
     get root_path
     assert_template 'static_pages/home'
+    # ヘッダーリンク
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    
+    # ボディのリンク
+    assert_select "a[href=?]", signup_path
+
+    # フッターのリンク
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
     
     get contact_path
     assert_select "title", full_title("Contact")
     
     get signup_path
     assert_select "title", full_title("Sign up")
-    
   end
+  
+  test "layout links when logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    #ヘッダーのリンク
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    #ボディのリンク
+    assert_select "a[href=?]", signup_path
+    #フッターのリンク
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+
 end
